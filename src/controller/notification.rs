63dd85d94abbde66a56d18db83b5d409bc6aeb52
unknown <hakimandi976@gmail.com>
2026-03-19 12:36:03 +0700

    Implement subscribe function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -4,3 +4,11 @@ use rocket::http::Status;
 use crate::model::subscriber::SubscriberRequest;
 use crate::model::notification::Notification;
 use crate::service::notification::NotificationService;
+
+#[post("/subscribe/<product_type>", data = "<subscriber>")]
+pub fn subscribe(product_type: String, subscriber: Json<SubscriberRequest>)
+    -> status::Custom<Json<SubscriberRequest>>
+{
+    NotificationService::subscribe(product_type, subscriber.clone().into_inner());
+    status::Custom(Status::Ok, subscriber)
+}
