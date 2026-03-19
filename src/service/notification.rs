b9496c4fb0b48e3e522f7d876dbe5f35377b5045
unknown <hakimandi976@gmail.com>
2026-03-19 12:36:45 +0700

    Implement unsubscribe function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -25,4 +25,24 @@ impl NotificationService {
             rt.block_on(Self::subscribe_request(product_type_clone, subscriber_clone));
         });
     }
+
+    async fn unsubscribe_request(product_type: String, subscriber: SubscriberRequest) {
+            let _ = REQWEST_CLIENT
+                .delete(format!("{}/notification/unsubscribe/{}?subscriber_url={}",
+                    bambangshop_receiver::APP_CONFIG.get_instance_root_url(),
+                    product_type,
+                    subscriber.url
+                ))
+                .send()
+                .await;
+        }
+
+        pub fn unsubscribe(product_type: String, subscriber: SubscriberRequest) {
+            let product_type_clone = product_type.clone();
+            let subscriber_clone = subscriber.clone();
+            std::thread::spawn(move || {
+                let rt = tokio::runtime::Runtime::new().unwrap();
+                rt.block_on(Self::unsubscribe_request(product_type_clone, subscriber_clone));
+            });
+        }
 }
