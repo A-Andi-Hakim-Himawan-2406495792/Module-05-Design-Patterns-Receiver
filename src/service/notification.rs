993d495eac50b0565f1d6383e75717cedc33b3f6
unknown <hakimandi976@gmail.com>
2026-03-19 12:35:24 +0700

    Implement subscribe function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -1,3 +1,4 @@
+use bambangshop_receiver::REQWEST_CLIENT;
 use crate::model::notification::Notification;
 use crate::model::subscriber::SubscriberRequest;
 use crate::repository::notification::NotificationRepository;
@@ -5,5 +6,23 @@ use crate::repository::notification::NotificationRepository;
 pub struct NotificationService;
 
 impl NotificationService {
+    async fn subscribe_request(product_type: String, subscriber: SubscriberRequest) {
+        let _ = REQWEST_CLIENT
+            .post(format!("{}/notification/subscribe/{}",
+                bambangshop_receiver::APP_CONFIG.get_instance_root_url(),
+                product_type
+            ))
+            .json(&subscriber)
+            .send()
+            .await;
+    }
 
+    pub fn subscribe(product_type: String, subscriber: SubscriberRequest) {
+        let product_type_clone = product_type.clone();
+        let subscriber_clone = subscriber.clone();
+        std::thread::spawn(move || {
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            rt.block_on(Self::subscribe_request(product_type_clone, subscriber_clone));
+        });
+    }
 }
